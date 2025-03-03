38b03603aace95fcc3486a705f84e5a832b97e18
EDN23 <e.salasfr23@gmail.com>
2025-03-03 07:52:01 -0600

    Actualización
    Anualicé los datos y agregué pesos aleatorios

--- Rendiminetos aritmeticos.py
+++ Rendiminetos aritmeticos.py
@@ -19,6 +19,7 @@ df["IPyC"] = pd.to_numeric(df["IPyC"], errors="coerce")  # Manejo de errores
 
 # Convertir Fecha a índice
 df = df.set_index("Fecha")
+df = df.drop("IPyC", axis=1)
 
 # Eliminar NaN
 df.dropna(inplace=True)
@@ -32,9 +33,9 @@ retornos_esp_anuales = retornos.mean() * 252 #anualizados
 retornos_esp_anuales_porc = round(retornos_esp_anuales*100,2)
  
 
-riesgo = retornos.std(ddof=0)*np.sqrt(252)
+riesgo = retornos.std(ddof=0)
 riesgo_anual = retornos.std(ddof=0)*np.sqrt(252)
-riesgo_anual_porc = round(retornos_esp_anuales*100,2)
+riesgo_anual_porc = round(riesgo_anual*100,2)
 
 # Imprimir resultados de cada uno de los activos
 print("Los rendimientos esperados anualizados de cada activo son:")
@@ -49,13 +50,39 @@ coef_var = round(riesgo_anual/retornos_esp_anuales,2)
 print(f"Los coeficientes de variacion son: \n{coef_var}")
 
 #Obtener la matriz de covarianzas
-matriz_cov = retornos.cov() * 252  # Matriz de covarianza anualizada
+matriz_cov = retornos.cov()
 
 #Graficar la matriz
 plt.figure(figsize=(8,6))
-sns.heatmap(matriz_cov, annot=True, fmt=".2f", cmap="coolwarm", linewidths=0.5)
+sns.heatmap(matriz_cov, annot=True, fmt=".5f", cmap="coolwarm", linewidths=0.5)
 plt.title("Matriz de Covarianza")
 plt.show()
+
+#Ahora hay que asignar pesos a los activos
+def generar_pesos(n):
+    pesos = np.random.rand(n)  # Genera n números aleatorios entre 0 y 1
+    pesos /= pesos.sum()  # Normaliza para que sumen 1
+    return pesos
+
+# Pesos aleatorios
+n_activos = len(retornos_esp_anuales)
+pesos_aleatorios = generar_pesos(n_activos)
+pesos_aleat_porc = pesos_aleatorios*100
+
+# Imprimir los pesos aleatorios junto a sus tickers
+print("Pesos Aleatorios:")
+for activo, peso in zip(retornos_esp_anuales.index, pesos_aleat_porc):
+    print(f"{activo}: {peso:.4f}%")
+print("Suma de Pesos:", round(pesos_aleatorios.sum(), n_activos))  # Debe ser 1
+
+#Obtener rendimiento y riesgo del portafolio
+rendimiento_portafolio = (pesos_aleatorios * retornos_esp_anuales).sum()
+print(f"Rendimiento esperado del portafolio: {rendimiento_portafolio:.4f}")
+
+
+
+
+
 
 
 
